d2c07b5fa18bf8187a8cd94716094e8304810120
Muhammad Esar <101944196+esarpeek@users.noreply.github.com>
2024-11-29 12:35:11 +0500

    Content Zooming Image

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3337526791"
+    serviceWorkerVersion: "1965812105"
   }
 });
