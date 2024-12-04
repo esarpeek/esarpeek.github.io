9d3a858ae24793c36f15ea052de10aa0659782b3
Muhammad Esar <101944196+esarpeek@users.noreply.github.com>
2024-12-04 12:17:08 +0500

    changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1965812105"
+    serviceWorkerVersion: "2411018628"
   }
 });
